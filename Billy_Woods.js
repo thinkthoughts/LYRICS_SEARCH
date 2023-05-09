721b7bba37c892d66b33a7dc3ece845deb54cb03
dan_hawkley <113205256+thinkthoughts@users.noreply.github.com>
2023-05-09 14:40:54 -0600

    Create Billy_Woods.js
    Backwoodz records

---
+++ Billy_Woods.js
@@ -0,0 +1,120 @@
+
+..........................................................................................................................
+                                                                                 
+ ```///   ) )                  ```||   //|  ///                        ///        
+   ///___/ /  ( ) // //           ||  // | ///  ___      ___      ___ ///  ___    
+  // __  (   / / // // //   ///   || ///||/// //   ) ) //   ) ) //   )// ((   ) ) 
+ ///    ) ) / / // // ((___///    ||/// | // //   / / //   / / //   ///   \ \     
+///____/ / /_/ // // _    ///     |_//  |_/ ((___/ / ((___/ / ((___/// //___) )   
+                    //   ///
+                   //___///
+                   \_ _ _/
+
+[Verse 1: billy woods]
+Quinine powder and alcohol, stir until dissolved
+The future isn't flying cars, it's Rachel Dolezal absolved
+It's autonomous computers sendin' shooters back in time at the behest of defunct message boards
+Translucent man-of-war, the tentacles caress my paramour
+Implored the bitch to be sensible
+The slave master's children all looked identical
+True, the future is children, but whatever future you buildin' already look misеrable
+Natural beauty, Savage Fеnty, we strip mined the minerals
+The trailer is the movie, Bobby Digital
+
+
+[Refrain: billy woods]
+Said he had a vision, said he had a system
+Said he was the one true and living, said he had a mission
+Said we in a prison
+Said he had a vision, said he had a system
+Said he was the one true and living, said he had a mission
+Said we in a prison
+
+[Verse 2: ELUCID]
+I do a dance from '86, I'm a simple machine
+Hammering the myth, pullin' it from the extreme
+The only answer sits, sits right in between
+She been playin' in my scalp 'bout an hour it seem
+In a place called now buyin' flowers and leaves
+Black soap, crack, choke under masonic sword
+From where they tax hold two thousand dollar reward
+Found on the road, dead, pulling cotton from thorns
+Scary hour, heretic, kicks canary
+Travel Fox, wouldn't step in blood, wouldn't [?]
+Bend the bitch one-eighty, what she asked for
+I been playin' in her ass more
+Hand of God, slept walked from Land of Nod
+Heavy lidded, past door front, tabernacle door, seeing everything
+Unknown pleasure from the belly sing
+My name already ring
+My neighbor never a word but always listening
+Thin walls, scream a full word
+No convincin' 'em
+You might also like
+Sauvage
+​billy woods
+The Doldrums
+​billy woods
+Christine
+​billy woods
+[Refrain: ELUCID]
+Akh said he had a vision, said he had a system
+Said he was the one true and living, said he had a mission
+Said we in a prison
+Said he had a vision, said he had a system
+Said he was the one true and living, said he had a mission
+Said we in a prison
+Said he had a vision, say he had a system
+Said he was the one true and living, said he had a mission
+Said we in a prison
+
+[Verse 3: Denmark Vessey]
+Medu Neter in a love letter
+Dove tears in the inkwell for a dove feather
+I make a dovetail when I spread the bills, blood money
+Heard blood sells, said it was a gimmick
+Sex sells, the Lord said I must replenish
+Get out my light, you already in my image
+My eyes can meet your twinkle and glisten
+Jeremy Scotts on my ankle, in prison
+This the flow that got Ponzi scheme written all over it
+Seen the ball of black slime, I pump fake a whole [piston?]
+Got infrared Jesus posted jumpin' out the prism
+Primary colors for prescription
+I prefer my bluntin' smothered in addiction (Yeah)
+Subjugation is the mother of invention
+Someone's pain is some other motherfucker's pension
+Your daddy was a rich man, stole an invention
+[Refrain: Denmark Vessey & Quelle Chris]
+Said he had a vision, said he had a mission (Said he had a)
+Said he was the one true and living, said he had a system (Said he had a system)
+Said he had a mission (Said he had a mission)
+(Said he had a mission)
+Said he had a (Said he had a)
+Said he had a
+
+[Verse 4: Quelle Chris]
+Said he had a mission
+Some niggas run when they see niggas runnin'
+Some niggas run and see
+I be the latter
+Four thousand years after King James claimed God's great (Who, me?)
+I said I'm flattered, time hoppin' like Peabody and Sherm
+Eyes bleedin' like Giuliani, peaking, felt little concern
+My imperfection's impressive, take L's to the chest like Laverne
+Used to rock a tar baby perm, Brer Rabbit habit
+Moves documented by the coons
+And all the gatekeepers walk pageant
+Ooh, delicioso, fresh madness, steamin'
+Right up out the pot, no seasonin'
+E'rybody wanna be the next big thing to pop out Mother Nature's gato breathin'
+Spells, chants, incantations, readings
+Yeah, nigga, it's all that
+Niggas thought we was writin' raps and we manipulate seasons
+That's no cap and all caps at the same time
+[?] Maasai walks the same line
+Canonically and sonically, I was in the same mind
+Don't need four or five g's to call shots like a land line
+Straight from the spine, handle mine
+Sidesteppin' like it's hammer time
+Deep cut so fire that it cauterizes
